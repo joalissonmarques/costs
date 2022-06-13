c28493d013a1d3f1406b99c9a6887d29fe0e7fa0
Joalisson Marques <joalisonmarques@hotmail.com>
2022-06-13 17:00:35 -0300

    Criado o navbar e footer

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 /* Container From Layout */
 import Container from './components/layout/Container'
+/* Import Navbar and Footer */
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-        <li>Empresa</li>
-        <li>Novo Projeto</li>
-      </ul>
-      <Container customClass="min-height">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Company" element={<Company />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/NewProject" element={<NewProject />} />
-        </Routes>
-      </Container>
-      <p>Footer</p>
+      <Navbar />
+        <Container customClass="min-height">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Company" element={<Company />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/NewProject" element={<NewProject />} />
+          </Routes>
+        </Container>
+      <Footer />
     </Router>
   )
 }
