f0d2c7320215877f63e469de0365f3f46d2529e7
Joalisson Marques <joalisonmarques@hotmail.com>
2022-06-13 15:18:18 -0300

    Criação de Container - Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+/* Container From Layout */
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+        <li>Empresa</li>
+        <li>Novo Projeto</li>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Company" element={<Company />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/NewProject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   )
 }
 
-export default App;
+export default App
