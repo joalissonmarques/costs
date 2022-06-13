0d8e5ad982156c9dac7f98a531d4751911c92d22
Joalisson Marques <joalisonmarques@hotmail.com>
2022-06-13 20:47:01 -0300

    Create Page NewProject

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ function Home(){
     <section className={styles.home_container}>
       <h1>Bem-vindo ao <span>Costs</span></h1>
       <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-      <LinkButton to="/NewProjects" text="Criar Projetos"/>
+      <LinkButton to="/NewProject" text="Criar Projetos"/>
       <img src={savings} alt="Costs"/>
     </section>
   )
