112f2bd4715fea36a8b59a37948d9cae68ab3763
Joalisson Marques <joalisonmarques@hotmail.com>
2022-06-15 19:42:28 -0300

    Criação de conexão com o db.json

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}) {
+function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() =>{
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(resp => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -11,8 +28,13 @@ function ProjectForm({btnText}) {
         text="Nome do Projeto"
         placeholder="Insira o nome do Projeto"
       />
-      <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o valor do projeto" />
-      <Select name="category_id" text="Selecione a categoria"/>
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        name="budget"
+        placeholder="Insira o valor do projeto"
+      />
+      <Select name="category_id" text="Selecione a categoria" options={categories} />
       <SubmitButton text={btnText} />
     </form>
   )
