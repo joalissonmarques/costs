b8736386291fd1ad291820ef6a83505b9366118d
Joalisson Marques <joalisonmarques@hotmail.com>
2022-06-16 20:00:57 -0300

    create config page form

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,6 +1,6 @@
 import styles from './Input.module.css'
 
-function Input({ type, text, name, placeholder, handleOnChange, value }) {
+function Input({ type, name, text, placeholder, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
